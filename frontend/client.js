67b52a1c9983cd3d26420f86c85c34c6dc28ca3c
faren-amri <faren.amri@gmail.com>
2025-05-11 10:41:35 +0100

    Clean working version of Power-Up with correct paths

--- frontend/client.js
+++ frontend/client.js
@@ -3,12 +3,12 @@ const TrelloPowerUp = window.TrelloPowerUp;
 TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
-      icon: 'https://itero-powerup.netlify.app/icon.png', // absolute URL
+      icon: 'https://itero-powerup.netlify.app/icon.png',
       text: 'Open Itero',
       callback: function (t) {
         return t.popup({
-          title: 'Complete Challenge',
-          url: 'https://itero-powerup.netlify.app/popup.html', // absolute URL
+          title: 'Challenge Popup',
+          url: 'https://itero-powerup.netlify.app/popup.html',
           height: 200
         });
       }

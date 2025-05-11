19435aa2e322718a4451df5082a975ad86dc61d6
faren-amri <faren.amri@gmail.com>
2025-05-11 14:40:36 +0100

    test routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,5 +1,7 @@
 from app.challenges.routes import challenge_bp
+from app.main.routes import main_bp  # ✅ new import
 
 def register_routes(app):
+    app.register_blueprint(main_bp)  # ✅ new line
     app.register_blueprint(challenge_bp, url_prefix="/api/challenges")
     return app

1e330058c13a4dfb8dfbae25b0254cf4229eecea
faren-amri <faren.amri@gmail.com>
2025-05-13 11:55:24 +0100

    Fix folder structure and deploy working frontend

--- backend/app/tasks/models.py
+++
@@ -1,8 +0,0 @@
-from app.database.db import db
-from datetime import datetime
-
-class TaskCompletion(db.Model):
-    __tablename__ = 'task_completion'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    completed_at = db.Column(db.DateTime, default=datetime.utcnow)

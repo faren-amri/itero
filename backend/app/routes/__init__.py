a2a2e1f4125bc7dbcd68d7f96d45b4ab41353a95
faren-amri <faren.amri@gmail.com>
2025-05-13 14:07:00 +0100

    Restore backend from commit dbe2ea0d

---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,23 @@
+from app.challenges.routes import challenge_bp
+from app.main.routes import main_bp  # ✅ new import
+from app.streaks.routes import streak_bp
+from app.tasks.routes import task_bp
+from app.moods.routes import mood_bp
+from app.dashboard.routes import dashboard_bp
+
+
+
+
+
+def register_routes(app):
+    app.register_blueprint(main_bp)  # ✅ new line
+    app.register_blueprint(challenge_bp, url_prefix="/api/challenges")
+    app.register_blueprint(streak_bp, url_prefix="/api/streaks")
+    app.register_blueprint(task_bp, url_prefix="/api/tasks")
+    app.register_blueprint(mood_bp, url_prefix="/api/mood")
+    app.register_blueprint(dashboard_bp, url_prefix="/api/dashboard")
+
+
+
+
+    return app

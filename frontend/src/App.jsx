c333c57295cf196df3ec8b29cf05ea27758014c2
faren-amri <faren.amri@gmail.com>
2025-05-13 15:00:09 +0100

    Fix React routing with HashRouter for Netlify deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MotivationDashboardPage from './pages/MotivationDashboardPage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<div>Welcome to Itero Power-Up</div>} />
-      <Route path="/dashboard" element={<MotivationDashboardPage />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<div>Welcome to Itero Power-Up</div>} />
+        <Route path="/dashboard" element={<MotivationDashboardPage />} />
+      </Routes>
+    </HashRouter>
   );
 };
 

81f900edd004beefc166b29be2e3fee9e4dd2c05
faren-amri <faren.amri@gmail.com>
2025-05-13 09:46:47 +0100

    Make email field in User model nullable

--- backend/app/users/models.py
+++ backend/app/users/models.py
@@ -5,6 +5,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     trello_id = db.Column(db.String(100), unique=True, nullable=True) 
     username = db.Column(db.String(80), unique=False) 
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
     xp = db.Column(db.Integer, default=0)
     level = db.Column(db.Integer, default=1)

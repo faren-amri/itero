082eeadf7848cd5c84d2ee55d5ff2d0651b9f387
faren-amri <faren.amri@gmail.com>
2025-05-15 09:20:43 +0100

    Fix Power-Up routing and iframe loading for Trello integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MotivationDashboardPage from './pages/MotivationDashboardPage';
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<div>Welcome to Itero Power-Up</div>} />
-        <Route path="/dashboard" element={<MotivationDashboardPage />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<MotivationDashboardPage />} />
+      <Route path="/dashboard" element={<MotivationDashboardPage />} />
+    </Routes>
   );
 };
 

349e0624e84db58d6391edaee9283d30096e227e
faren-amri <faren.amri@gmail.com>
2025-05-15 19:45:26 +0100

    Fix: move HashRouter to main.jsx to prevent nested routers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MotivationDashboardPage from './pages/MotivationDashboardPage';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/dashboard" element={<MotivationDashboardPage />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/dashboard" element={<MotivationDashboardPage />} />
+    </Routes>
   );
 }
 

95fb1c4d41f36aa3477a13cdbb5c8be0e8f1cd9a
faren-amri <faren.amri@gmail.com>
2025-05-25 16:50:52 +0100

    Fix: Add CORS headers for Netlify frontend

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
+# config.py
 import os
 
 class Config:
-    raw_url = os.environ.get("MYSQL_URL")
-    SQLALCHEMY_DATABASE_URI = raw_url.replace("mysql://", "mysql+pymysql://", 1) if raw_url else None
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False

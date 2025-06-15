64e74881c9c4e27328ff433d97dff8ecf277ea54
faren-amri <faren.amri@gmail.com>
2025-06-15 18:11:17 +0100

    Icon fix1

--- frontend/dist/client.js
+++ frontend/dist/client.js
@@ -3,12 +3,12 @@ const TrelloPowerUp = window.TrelloPowerUp;
 TrelloPowerUp.initialize({
   'card-buttons': function (t) {
     return [{
-      icon: 'https://itero-powerup.netlify.app/icon.png',
+      icon: 'https://itero-powerup.netlify.app/assets/itero-icon-144.png',
       text: 'Motivation Dashboard',
       callback: function (t) {
         return t.popup({
           title: 'Motivation Dashboard',
-          url: 'index.html',
+          url: '/popup.html', 
           height: 400
         });
       }

b51100d257fd44fe1268da74b029ee82d7269041
faren-amri <faren.amri@gmail.com>
2025-08-30 11:19:35 +0100

    version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import replace from '@rollup/plugin-replace'
 
 export default defineConfig({
   base: './',
-  plugins: [react()]
-  // ← no build.rollupOptions.input; Vite will copy /public/* to dist root
+  plugins: [
+    react(),
+    replace({
+      preventAssignment: true,
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
+    })
+  ]
 })

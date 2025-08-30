8af864ef457850d24861a03abdafb05b6d69baab
faren-amri <faren.amri@gmail.com>
2025-08-30 10:39:09 +0100

    dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        popup: 'public/popup.html'
-      }
-    }
-  }
+  plugins: [react()]
+  // ← no build.rollupOptions.input; Vite will copy /public/* to dist root
 })
